7390a3f2b1bb1c8e98c740447a022e0fba2a6772
patrick brisbin <pbrisbin@gmail.com>
2015-09-16 14:25:55 -0400

    Stripped scaffold

---
+++ Handler/Home.hs
@@ -0,0 +1,6 @@
+module Handler.Home where
+
+import Import
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ $(widgetFile "homepage")
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "tee-io" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
